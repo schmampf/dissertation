8b54cb1d77381e32b6f38270f5ed4eb7a29d0822
schmampf <oliver.irtenkauf@uni-konstanz.de>
2026-01-06 21:39:18 +0100

    implemented subfigure

--- .utilities/thesislayout.py
+++ .utilities/thesislayout.py
@@ -8,6 +8,7 @@ from matplotlib.pyplot import Figure, Axes
 def get_figure(
     figsize=(1.7, 0.85),
     facecolor=None,
+    subfigure=False,
 ):
     fig, ax = plt.subplots(figsize=figsize, facecolor=facecolor)
     ax.tick_params(
@@ -17,6 +18,14 @@ def get_figure(
         labelsize=8,
         pad=1.5,
     )
+    ax.subfigure: bool = subfigure
+    if ax.subfigure:
+        labelsize = 7
+        ax.xaxis.label.set_size(7)
+        ax.yaxis.label.set_size(7)
+        ax.tick_params(labelsize=labelsize)
+    else:
+        labelsize = 8
 
     # Remove frame
     for spine in ["top", "right"]:
@@ -27,7 +36,7 @@ def get_figure(
         axis="both",
         direction="out",
         length=3,
-        labelsize=8,
+        labelsize=labelsize,
         color="k",
         labelcolor="k",
     )
@@ -38,19 +47,36 @@ def theory_layout(
     fig: Figure,
     ax: Axes,
     title: Optional[str] = None,
-    xlabel: str = "$x$",
-    ylabel: str = "$y$",
-    padding: tuple[float, float] = (0.3, 0.15),
-    base_padding: tuple[float, float] = (0.08, 0.08),
+    xlabel: Optional[str] = None,
+    ylabel: Optional[str] = None,
+    padding: Optional[tuple[float, float]] = None,
+    base_padding: Optional[tuple[float, float]] = None,
 ):
+    if xlabel is None:
+        xlabel = "$x$"
+
+    if ylabel is None:
+        ylabel = "$y$"
+
+    if padding is None:
+        padding = (0.3, 0.2)
+
+    if base_padding is None:
+        base_padding = (0.08, 0.08)
+
+    ms: float = 6.0
+    fontsize: float = 8
+    if ax.subfigure:
+        ms: float = 4.0
+        fontsize: float = 7
 
     # get limits
     x0, x1 = ax.get_xlim()
     y0, y1 = ax.get_ylim()
 
     # Draw arrow heads
-    ax.plot(x1, y0, ">", color="k", clip_on=False)
-    ax.plot(x0, y1, "^", color="k", clip_on=False)
+    ax.plot(x1, y0, ">", color="k", ms=ms, clip_on=False)
+    ax.plot(x0, y1, "^", color="k", ms=ms, clip_on=False)
 
     # get config stuff
     w0, h0 = fig.get_size_inches()
@@ -73,12 +99,14 @@ def theory_layout(
 
     # labels
     ax.text(
-        x1 + bpx / w1 * dx,
-        y0 - (py + bpy) / h1 * dy,
-        xlabel,
-        ha="right",
+        # x = x1 + bpx / w1 * dx, #label on the right
+        # ha="right",
+        x=x0 + dx / 2,  # label in the middle
+        ha="center",
+        y=y0 - (py + bpy) / h1 * dy,
+        s=xlabel,
         va="bottom",
-        fontsize=8,
+        fontsize=fontsize,
     )
     ax.text(
         x0 - (bpx + px) / w1 * dx,
@@ -87,7 +115,7 @@ def theory_layout(
         rotation=90,
         ha="left",
         va="center",
-        fontsize=8,
+        fontsize=fontsize,
     )
 
     # set limits
